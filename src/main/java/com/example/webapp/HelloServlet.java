2739a03644cfce462cb621d76e8f9c7bec9fc01c
Ruvinda Dilshan <bmruvinda2001@gmail.com>
2024-04-08 16:40:29 +0530

    Created admin dashboards basic Add product function and show the database contents.

--- src/main/java/com/example/webapp/HelloServlet.java
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -4,11 +4,15 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.sql.SQLException;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import javax.servlet.http.Part;
 import javax.servlet.annotation.MultipartConfig;
+
+import static com.example.webapp.dbConnection.fetchAllFromDb;
 
 @MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
         maxFileSize=1024*1024*10,      // 10MB
@@ -16,6 +20,13 @@ import javax.servlet.annotation.MultipartConfig;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        List<Map<String, Object>> data = fetchAllFromDb();
+        request.setAttribute("data", data);
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
+    }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 //
@@ -34,6 +45,11 @@ public class HelloServlet extends HttpServlet {
             Files.copy(fileContent, Paths.get(uploadDirectory + fileName));
 
             dbConnection.insertToDb(Name, brand, price, description, fileName);
+
+           // response.sendRedirect("index.jsp");
+
+
+
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/main/java/com/example/webapp/dbConnection.java
+++ src/main/java/com/example/webapp/dbConnection.java
@@ -1,9 +1,10 @@
 package com.example.webapp;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class dbConnection {
     public static void insertToDb(String productname, String brand, String price, String description, String fileName) {
@@ -24,23 +25,38 @@ public class dbConnection {
         }
     }
 
-//    public static void getFromDb(String uname, String pass) {
-//        String driver = "com.mysql.cj.jdbc.Driver";
-//        String url = "jdbc:mysql://localhost:3306/admin";
-//        String query = "INSERT INTO adminlogin (username, password) VALUES ('"+ uname +"', '"+ pass +"')";
-//
-//        try {
-//            Class.forName(driver);
-//            Connection con = DriverManager.getConnection(url,"root","");
-//            Statement st = con.createStatement();
-//            st.executeUpdate(query);
-//            System.out.println("MySQL JDBC Driver is in the classpath.");
-//
-//        } catch (ClassNotFoundException | SQLException e) {
-//            System.out.println("MySQL JDBC Driver is not in the classpath.");
-//            throw new RuntimeException(e);
-//        }
-//    }
+    public static List<Map<String, Object>> fetchAllFromDb() {
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/webapp";
+        String query = "SELECT * FROM product";
+
+        List<Map<String, Object>> resultList = new ArrayList<>();
+
+        try {
+            Class.forName(driver);
+            Connection con = DriverManager.getConnection(url,"root","");
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                Map<String, Object> row = new HashMap<>();
+                row.put("ProductName", rs.getString("ProductName"));
+                row.put("Brand", rs.getString("Brand"));
+                row.put("Price", rs.getString("Price"));
+                row.put("Description", rs.getString("Description"));
+                row.put("Image", rs.getString("Image"));
+                resultList.add(row);
+            }
+
+            System.out.println("MySQL JDBC Driver is in the classpath.");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("MySQL JDBC Driver is not in the classpath.");
+            throw new RuntimeException(e);
+        }
+
+        return resultList;
+    }
 //
 //    public static void updateToDb(String uname, String pass) {
 //        String driver = "com.mysql.cj.jdbc.Driver";
