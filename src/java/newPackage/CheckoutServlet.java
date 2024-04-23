be5d7eaf41a7ba6841213d7e26c41f5535b1e9e3
Faraz Weerabangsa <136410455+FarazWeerabangsa@users.noreply.github.com>
2024-04-23 22:20:37 +0530

    create checkout class objects

--- src/java/newPackage/CheckoutServlet.java
+++ src/java/newPackage/CheckoutServlet.java
@@ -1,9 +1,7 @@
-
 package newPackage;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.*;
+import java.util.Random;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,26 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 public class CheckoutServlet extends HttpServlet {
 
   
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-           
-        }
-    }
-
-   
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-   
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException 
     {
+        
         String name = request.getParameter("name");
         String address = request.getParameter("address");
         String city = request.getParameter("city");
@@ -48,57 +31,24 @@ public class CheckoutServlet extends HttpServlet {
         String[] quantities = request.getParameterValues("quantities");
         double subtotal = Double.parseDouble(request.getParameter("subtotal"));
         
-        PrintWriter out = response.getWriter();
+        Random random = new Random();
+     
+        int randomNo = random.nextInt(9000) + 1000;
+        int orderID = Integer.parseInt( String.valueOf(randomNo)+String.valueOf(uid));
         
-        String url = "jdbc:mysql://localhost:3306/store";
+       
+        
+        Checkout checkout =new Checkout();
+        
+        checkout.UpdateUser(uid, name, address, city, zipcode, phoneno, email);
+   
+        checkout.InsertOrder(orderID, uid, productIds, quantities, subtotal, date);
 
-// SQL statement for updating the Information table
-String updateSql = "UPDATE information " +
-                        "SET UName=?, Address=?, City=?, Zipcode=?, PhoneNO=?, OEmail=? " +
-                        "WHERE UserID=?";
 
-// SQL statement for inserting values into the Order table
-String insertSql = "INSERT INTO orders (UserID, PID, Quantity, Subtotal, Date) " +
-                        "VALUES (?, ?, ?, ?, ?)";
-
-try (
-    
-    Connection conn = DriverManager.getConnection(url, "root", "");
-    // Create prepared statements
-    PreparedStatement pstmt1 = conn.prepareStatement(updateSql);
-    PreparedStatement pstmt2 = conn.prepareStatement(insertSql)
-) {
-    
-    pstmt1.setString(1, name);
-    pstmt1.setString(2, address);
-    pstmt1.setString(3, city);
-    pstmt1.setString(4, zipcode);
-    pstmt1.setString(5, phoneno);
-    pstmt1.setString(6, email);
-    pstmt1.setInt(7, uid);
-
-    
-    int rowsAffected = pstmt1.executeUpdate();
-    System.out.println("Rows affected in Information table: " + rowsAffected);
-            
-    
-    for (int i = 0; i < productIds.length; i++) {
-        pstmt2.setInt(1, uid);
-        pstmt2.setInt(2, Integer.parseInt(productIds[i]));
-        pstmt2.setInt(3, Integer.parseInt(quantities[i]));
-        pstmt2.setDouble(4, subtotal);
-        pstmt2.setString(5, date);
-        pstmt2.addBatch();
-    }
-    int[] rowsAffectedBatch = pstmt2.executeBatch();
-    System.out.println("Rows affected in Order table: " + rowsAffectedBatch.length);
-} catch (SQLException e) {
-    e.printStackTrace();
-}
   
         response.sendRedirect("Products.jsp");
     }
-
+   
     
     @Override
     public String getServletInfo() {
