cc7555f5b345a8727f9df3680d5ef58b210c4bb6
Dillon Williams <136365918+DillonWilliams1@users.noreply.github.com>
2024-04-26 11:29:03 +0530

    create method to get data

--- src/java/mypackage/Order.java
+++ src/java/mypackage/Order.java
@@ -32,7 +32,45 @@ public class Order {
         }
     }
 
-   
+    public List<OrderDetails> getOrders(int userId) {
+    List<OrderDetails> orders = new ArrayList<>(); 
+    
+    try {
+        connectDB();
+        String query = "SELECT p.ProductName, p.Price, p.Image, o.Date, o.Subtotal, o.OrderId, o.Quantity ,u.Address, u.PhoneNO " +
+                           "FROM orders o " +
+                           "JOIN information u ON o.UserID = u.UserID " +
+                           "JOIN product p ON o.ProductID = p.ProductID " +
+                           "WHERE o.UserID = ?";
+
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, userId);
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                String productName = rs.getString("ProductName");
+                double productPrice = rs.getDouble("Price");
+                String date = rs.getString("Date");
+                double subtotal = rs.getDouble("Subtotal");
+                int orderId = rs.getInt("OrderId");
+                int quantity = rs.getInt("Quantity");
+                String image = rs.getString("Image");
+                String address = rs.getString("Address");
+                String phoneno = rs.getString("PhoneNo");
+
+                OrderDetails order = new OrderDetails(productName, productPrice, date, subtotal, orderId, quantity, image, address, phoneno);
+                orders.add(order);
+            }
+        } 
+
+    } catch (SQLException ex) {
+        Logger.getLogger(Order.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    
+    return orders;  
+}
+
+    
     }
 
 
