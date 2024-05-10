2bc97b4a15e15ed8deb30134ed740de8388cf9d5
Prasanna BMRD <67103590+rudill@users.noreply.github.com>
2024-05-10 19:57:28 +0530

    Added dispatchers to servlet

--- src/main/java/com/example/webapp/HelloServlet.java
+++ src/main/java/com/example/webapp/HelloServlet.java
@@ -1,34 +1,57 @@
-package com.example.webapp;
-import java.io.*;
+package Servlet;
+
+import Dao.productTableConnection;
+import model.orders;
+import model.productData;
+import model.userData;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.List;
-import java.util.Map;
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
-import javax.servlet.annotation.*;
-import javax.servlet.http.Part;
-import javax.servlet.annotation.MultipartConfig;
 
-import static com.example.webapp.productTableConnection.fetchAllFromDb;
+import static Dao.fetchOrder.fetchFromOrders;
+import static Dao.productTableConnection.fetchFromProduct;
+import static Dao.userTableConnection.fetchFromInformation;
 
 @MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
         maxFileSize=1024*1024*10,      // 10MB
         maxRequestSize=1024*1024*50)   // 50MB
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "helloServlet", value = "/hello-servlet"
+        , urlPatterns = {"/hello-servlet"}
+)
 public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-        List<Map<String, Object>> data = fetchAllFromDb();
-        request.setAttribute("data", data);
-        request.getRequestDispatcher("/index.jsp").forward(request, response);
+
+        List<productData> products = fetchFromProduct();
+        List <userData> user = fetchFromInformation();
+        List <orders> order = fetchFromOrders();
+
+        request.setAttribute("products", products);
+        request.setAttribute("user" , user);
+        request.setAttribute("order", order);
+
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
 
     }
 
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException  {
 
         productData getData = new productData();
 
@@ -38,23 +61,26 @@ public class HelloServlet extends HttpServlet {
                 getData.setPrice(request.getParameter("price"));
                 getData.setCategory(request.getParameter("category"));
                 getData.setDescription(request.getParameter("description"));
+                getData.setQuantity(request.getParameter("quantity"));
 
 
             Part filePart = request.getPart("image");
             getData.setFileName(Paths.get(filePart.getSubmittedFileName()).getFileName().toString());
             InputStream fileContent = filePart.getInputStream();
-            String uploadDirectory = "D:\\javaWebApps\\webApp\\src\\main\\webapp\\images\\";
+            String uploadDirectory = "D:\\javaWeb\\Store\\src\\main\\webapp\\images\\";
+
 
             Files.copy(fileContent, Paths.get(uploadDirectory + getData.getFileName()), StandardCopyOption.REPLACE_EXISTING);
 
 
-            productTableConnection.insertToDb(getData.getProductName(), getData.getBrand(), getData.getPrice(), getData.getCategory(), getData.getDescription(), getData.getFileName());
+            productTableConnection.insertTopProduct(getData.getProductName(), getData.getBrand(), getData.getPrice(), getData.getCategory(),
+                    getData.getDescription(), getData.getFileName(), getData.getQuantity());
 
             response.sendRedirect("index.jsp");
 
 
 
-            //response.sendRedirect("index.jsp");
+
 
             } catch (IOException e) {
                 e.printStackTrace();
@@ -76,7 +102,7 @@ public class HelloServlet extends HttpServlet {
             String method = request.getParameter("method");
             if("delete".equals(method)){
                 String ID = request.getParameter("ID");
-                productTableConnection.deleteFromDb(ID);
+                productTableConnection.deleteFromProduct(ID);
                 response.sendRedirect("index.jsp");
             }
             else if("update".equals(method)){
@@ -86,7 +112,7 @@ public class HelloServlet extends HttpServlet {
                 Part filePart = request.getPart("upImage");
                 updateData.setFileName(Paths.get(filePart.getSubmittedFileName()).getFileName().toString());
                 InputStream fileContent = filePart.getInputStream();
-                String uploadDirectory = "D:\\javaWebApps\\webApp\\src\\main\\webapp\\images\\";
+                String uploadDirectory = "D:\\javaWeb\\Store\\src\\main\\webapp\\images";
                 Files.copy(fileContent, Paths.get(uploadDirectory + updateData.getFileName()), StandardCopyOption.REPLACE_EXISTING);
 
 
@@ -96,7 +122,7 @@ public class HelloServlet extends HttpServlet {
                 updateData.setPrice(request.getParameter("price"));
                 updateData.setCategory(request.getParameter("category"));
                 updateData.setDescription(request.getParameter("description"));
-                productTableConnection.updateToDb(updateData.getID(), updateData.getProductName(), updateData.getBrand(), updateData.getPrice(), updateData.getCategory(), updateData.getDescription(), updateData.getFileName());
+                productTableConnection.updateToProduct(updateData.getID(), updateData.getProductName(), updateData.getBrand(), updateData.getPrice(), updateData.getCategory(), updateData.getDescription(), updateData.getFileName());
                 response.sendRedirect("index.jsp");
             }
 
